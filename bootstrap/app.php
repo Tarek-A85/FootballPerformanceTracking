0e6dd714987072fa00157243fcf5d136f6ba9241
Tarek-A85 <ttallh1@gmail.com>
2025-01-29 18:44:50 +0300

    trying deployement

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'check_locale' => LocaleMiddleware::class,
             'is_admin' => CheckAdminMiddleware::class,
